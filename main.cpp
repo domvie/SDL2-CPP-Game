502c1b9f8eff8282068e850dea1f13e63ecae379
domvie <dominic.viehbock@gmail.com>
2019-05-05 22:59:48 +0200

    basic functionality added, creates blank window

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "game.h"
+
+Game *game = nullptr;
+
+
+using namespace std;
+
+int main(int argc, const char* argv[])
+{
+    game = new Game();
+    game->init("Dominics Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+    while(game -> running())
+    {
+        game->handleEvents();
+        game->update();
+        game->render();
+    }
+
+    game->clean();
+    return 0;
+
+}
